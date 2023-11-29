6aebb102e411d4fe06fbf994683acc2b0e789f0e
ShirinAziz <Shirin.aziz96@outlook.com>
2023-11-29 23:54:59 +0100

    user can now post  and get posts

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let express = require("express");
 let fs = require("fs");
 let app = express();
+const dbConnection = require("./db");
 let port = 8080;
 
 app.listen(port, function () {
@@ -10,15 +11,49 @@ app.listen(port, function () {
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/index.html");
+  fs.readFile(__dirname + "/index.html", "utf8", (err, data) => {
+    console.log("here");
+    if (err) {
+      console.error("Error reading HTML file:", err);
+      return res.status(500).send("Error reading HTML file");
+    }
+    dbConnection.query("SELECT * FROM users", function (err, result, fields) {
+      if (err) throw err;
+
+      let modifiedHTML = data.replace(
+        "<!-- Placeholders for data -->",
+        result
+          .toReversed()
+          .map(
+            (entry) => `
+        <div style="display: flex; flex-direction: column">
+          <div>Name: ${entry.name}</div> 
+          <div> Email: ${entry.email}</div> 
+          <div> Email: ${entry.phone}</div> 
+          <div> Email: ${entry.comment}</div> 
+        </div>
+        <hr/>
+      `
+          )
+          .join("")
+      );
+
+      res.send(modifiedHTML);
+    });
+  });
 });
 
 app.post("/", function (req, res) {
-  let html = fs.readFileSync("index.html").toString();
-  html = html.replace("**NAME**", req.body.name);
-  html = html.replace("**EMAIL**", req.body.email);
-  html = html.replace("**TEL**", req.body.tel);
-  html = html.replace("**COMMENT**", req.body.comment);
+  const { name, email, tel, comment } = req.body;
 
-  res.send(html);
+  dbConnection.query(
+    "INSERT INTO users (name, email, phone, comment) VALUES (?,?,?,?)",
+    [name, email, tel, comment],
+    function (err, result) {
+      if (err) throw err;
+      console.log(result);
+    }
+  );
+
+  res.redirect(303, "/");
 });
